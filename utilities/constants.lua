425057f9953dddcf12468123571cad330e3c5953
unknown <nick@salte.io>
2019-09-23 17:02:05 -0500

    v1.2.1

--- utilities/constants.lua
+++ utilities/constants.lua
@@ -2,7 +2,7 @@ local _, ns = ...
 
 local CONSTANTS = {
     DEFAULT_CONFIG = {
-        VERSION = "1.2.0",
+        VERSION = "1.2.1",
         PositionX = 0,
         PositionY = -240,
         Width = 250,

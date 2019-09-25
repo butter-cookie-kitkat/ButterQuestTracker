264cc10166cdba16fd07fed365ace7fabbc68483
unknown <nick@salte.io>
2019-09-25 18:44:36 -0500

    feat: added the ability to add support for different languages

---
+++ Locale/enUS/config.lua
@@ -0,0 +1,83 @@
+ButterQuestTrackerLocale.locale['enUS'] = {
+    -- Settings
+
+    ['SETTINGS_NAME'] = "Butter Quest Tracker %s",
+
+    -- Settings > Filters & Sorting
+
+    ['SETTINGS_FILTERS_AND_SORTING_TAB'] = "Filters & Sorting",
+
+    ['SETTINGS_CURRENT_ZONE_ONLY_NAME'] = "Current Zone Only",
+    ['SETTINGS_CURRENT_ZONE_ONLY_DESC'] = "Displays quests relevant to the current zone / subzone.",
+
+    ['SETTINGS_SORTING_NAME'] = "Sorting",
+    ['SETTINGS_SORTING_DESC'] = "Dictate how quests are sorted in the tracker.",
+
+    ['SETTINGS_SORTING_DISABLED_OPTION'] = "Don't Sort",
+    ['SETTINGS_SORTING_BY_LEVEL_OPTION'] = "By Level",
+    ['SETTINGS_SORTING_BY_LEVEL_REVERSED_OPTION'] = "By Level (Reversed)",
+    ['SETTINGS_SORTING_BY_PERCENT_COMPLETED_OPTION'] = "By %% Completed",
+
+    ['SETTINGS_HIDE_COMPLETED_QUESTS_NAME'] = "Hide Completed Quests",
+    ['SETTINGS_HIDE_COMPLETED_QUESTS_DESC'] = "Displays quests that have been completed.",
+
+    ['SETTINGS_QUEST_LIMIT_NAME'] = "Quest Limit",
+    ['SETTINGS_QUEST_LIMIT_DESC'] = "Limits the number of quests visible on the screen at a given time.",
+
+    ['SETTINGS_RESET_TRACKING_OVERRIDES_NAME'] = "Reset Tracking Overrides",
+    ['SETTINGS_RESET_TRACKING_OVERRIDES_DESC'] = "Reset's all manual quest watch overrides.",
+
+    -- Settings > Frame Settings
+
+    ['SETTINGS_FRAME_TAB'] = "Frame Settings",
+
+    ['SETTINGS_POSITIONX_NAME'] = "Position X",
+
+    ['SETTINGS_POSITIONY_NAME'] = "Position Y",
+
+    ['SETTINGS_WIDTH_NAME'] = "Width",
+
+    ['SETTINGS_MAX_HEIGHT_NAME'] = "Max Height",
+
+    ['SETTINGS_RESET_POSITION_NAME'] = "Reset Position",
+
+    ['SETTINGS_RESET_SIZE_NAME'] = "Reset Size",
+
+    -- Settings > Visual Settings
+
+    ['SETTINGS_VISUAL_TAB'] = "Visual Settings",
+
+    ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_NAME'] = "Color Headers By Difficulty Level",
+    ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_DESC'] = "Color codes the quests by their difficulty level.",
+
+    ['SETTINGS_TRACKER_HEADER_FORMAT_NAME'] = "Tracker Header Format",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_DESC'] = "How should we format the tracker header?",
+
+    ['SETTINGS_TRACKER_HEADER_FORMAT_CLASSIC_OPTION'] = "Classic (Disabled)",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_OPTION'] = "Quests",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_NUMBER_VISIBLE_OPTION'] = "Quests (10/18)",
+
+    ['SETTINGS_TRACKER_HEADER_FONT_SIZE_NAME'] = "Tracker Header Font Size",
+    ['SETTINGS_QUEST_HEADER_FONT_SIZE_NAME'] = "Quest Header Font Size",
+    ['SETTINGS_OBJECTIVE_FONT_SIZE_NAME'] = "Objective Font Size",
+
+    -- Settings > Advanced Settings
+
+    ['SETTINGS_ADVANCED_TAB'] = "Advanced",
+
+    ['SETTINGS_DEVELOPER_HEADER'] = "Developer Options",
+    ['SETTINGS_DEVELOPER_MODE_NAME'] = "Developer Mode",
+    ['SETTINGS_DEVELOPER_MODE_DESC'] = "Enables logging and other visual helpers.",
+
+    ['SETTINGS_DEBUG_LEVEL_NAME'] = "Debug Level",
+
+    ['SETTINGS_LOCALE_HEADER'] = "Localization Settings",
+    ['SETTINGS_LOCALE_NAME'] = "Select UI Locale",
+
+    ['SETTINGS_RESET_HEADER'] = "Reset Butter Quest Tracker",
+    ['SETTINGS_RESET_TEXT'] = "Hitting this button will reset all Butter Quest Tracker configuration settings back to their default values.",
+
+    ['SETTINGS_RESET_NAME'] = "Reset Settings",
+    ['SETTINGS_RESET_DESC'] = "Reset all of Butter Quest Tracker's settings to their default values.",
+    ['SETTINGS_ADVERT_TEXT'] = "|c00FF9696Butter Quest Tracker is under active development for World of Warcraft: Classic. Please check out our GitHub for the alpha builds or to report issues. \n\nhttps://github.com/butter-cookie-kitkat/ButterQuestTracker"
+};

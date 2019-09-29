e4c8d73b95358f5af68d815172d86a322a211508
unknown <nick@salte.io>
2019-09-28 21:05:21 -0500

    v1.8.0-rc.1

--- utilities/constants.lua
+++ utilities/constants.lua
@@ -1,7 +1,7 @@
 local NAME, ns = ...
 
 local CONSTANTS = {
-    VERSION = "1.8.0-beta.2",
+    VERSION = "1.8.0-rc.1",
     NAME = "Butter Quest Tracker",
     NAME_SQUASHED = "ButterQuestTracker",
     CURSEFORGE_SLUG = "butter-quest-tracker",

e19b6a67ab2ee661d348c165d85e8a949be6439c
Ceci <admin@cecilias.me>
2019-10-04 21:21:05 -0500

    fix: resolved an issue where various frames would throw errors about blocked actions upon being opened
    - Specifically if you opened the Spellbook frame while in combat (because that makes sense right?)
    
    See #53 for more info and insanity

--- TrackerHelper/Modules/Base.lua
+++ TrackerHelper/Modules/Base.lua
@@ -2,6 +2,9 @@ local CreateClass = LibStub("Poncho-1.0");
 
 TrackerHelperBase = CreateClass("Frame", "TrackerHelperBase");
 local Base = TrackerHelperBase;
+local cache = {
+    hex = {}
+};
 
 function Base:_distance(x1, y1, x2, y2)
 	return math.sqrt( (x2-x1)^2 + (y2-y1)^2 );
@@ -16,33 +19,40 @@ function Base:_count(t)
 end
 
 function Base:_hex2rgb(hex)
-    hex = hex:gsub("#","")
-    if #hex == 8 then
-        return {
-            r = tonumber("0x" .. hex:sub(3,4)) / 255,
-            g = tonumber("0x" .. hex:sub(5,6)) / 255,
-            b = tonumber("0x" .. hex:sub(7,8)) / 255,
-            a = tonumber("0x" .. hex:sub(1,2)) / 255
-        };
+    if not cache.hex[hex] then
+        hex = hex:gsub("#", "");
+
+        if #hex == 8 then
+            cache.hex[hex] = {
+                r = tonumber("0x" .. hex:sub(3,4)) / 255,
+                g = tonumber("0x" .. hex:sub(5,6)) / 255,
+                b = tonumber("0x" .. hex:sub(7,8)) / 255,
+                a = tonumber("0x" .. hex:sub(1,2)) / 255
+            };
+        else
+            cache.hex[hex] = {
+                r = tonumber("0x" .. hex:sub(1,2)) / 255,
+                g = tonumber("0x" .. hex:sub(3,4)) / 255,
+                b = tonumber("0x" .. hex:sub(5,6)) / 255,
+                a = 1.0
+            };
+        end
     end
 
-    return {
-        r = tonumber("0x" .. hex:sub(1,2)) / 255,
-        g = tonumber("0x" .. hex:sub(3,4)) / 255,
-        b = tonumber("0x" .. hex:sub(5,6)) / 255,
-        a = 1.0
-    };
+    return cache.hex[hex];
 end
 
 function Base:_normalizeColor(value)
+    if not value then return end
+
     if type(value) == "string" then
         value = self:_hex2rgb(value);
-    elseif type(value) == "table" then
-        value.r = value.r or 0.0;
-        value.g = value.g or 0.0;
-        value.b = value.b or 0.0;
-        value.a = value.a or 1.0;
     end
 
-    return value;
+    return {
+        r = value.r or 0.0,
+        g = value.g or 0.0,
+        b = value.b or 0.0,
+        a = value.a or 1.0,
+    };
 end

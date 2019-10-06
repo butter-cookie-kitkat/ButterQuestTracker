30361806e722827efa0668815e654b00b0b6e7b1
Nick Woodward <nick@salte.io>
2019-10-06 00:36:06 -0500

    chore: resolved luacheck errors

--- LibQuestHelpers/LibQuestHelpers.lua
+++ LibQuestHelpers/LibQuestHelpers.lua
@@ -1,7 +1,6 @@
 local ZH = LibStub("ZoneHelper-1.0");
 local QLH = LibStub("QuestLogHelper-1.0");
 local QWH = LibStub("QuestWatchHelper-1.0");
-local HBD = LibStub("HereBeDragons-2.0");
 local helper = LibStub:NewLibrary("LibQuestHelpers-1.0", 1);
 local isWoWClassic = select(4, GetBuildInfo()) < 20000;
 
@@ -32,26 +31,6 @@ end
 
 local function getDistance(x1, y1, x2, y2)
 	return math.sqrt( (x2-x1)^2 + (y2-y1)^2 );
-end
-
-local zoneCache = {};
-local function zoneNameToUIMapID(zone)
-    if not zoneCache[zone] then
-        local index = 1;
-        local mapIDs = HBD:GetAllMapIDs();
-
-        while not zoneCache[zone] and index <= table.getn(mapIDs) do
-            local mapID = mapIDs[index];
-
-            if HBD:GetLocalizedMap(mapID) == zone then
-                zoneCache[zone] = mapID;
-            end
-
-            index = index + 1;
-        end
-    end
-
-    return zoneCache[zone];
 end
 
 function helper:GetAddons()

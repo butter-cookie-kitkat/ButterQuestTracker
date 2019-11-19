822777bd6f9e92e122779a26ea26512c9cc587e2
yaoenqi <yaoenqi@163.com>
2019-11-19 10:17:34 +0800

    Create Config.zhCN.lua

---
+++ Locale/Config.zhCN.lua
@@ -0,0 +1,123 @@
+ButterQuestTrackerLocale.locale['zhCN'] = {
+    -- Settings 设置
+
+    ['SETTINGS_NAME'] = "Butter Quest Tracker |cFFFFFFFFv%s|r",
+    ['SETTINGS_ENABLED_NAME'] = "启用",
+    ['SETTINGS_FORMAT_NAME'] = "格式",
+
+    ['SETTINGS_DISPLAY_DUMMY_DATA_NAME'] = "显示虚拟数据",
+    ['SETTINGS_DISPLAY_DUMMY_DATA_DESC'] = "在追踪器中显示虚拟数据，使您可以更容易地调整视觉效果。这就好像你现在的区域是雷霆崖一样。",
+
+    -- Settings 设置 > Filters & Sorting 过滤和排序
+
+    ['SETTINGS_FILTERS_AND_SORTING_TAB'] = "过滤和排序",
+
+    ['SETTINGS_AUTO_TRACK_UPDATED_QUESTS_NAME'] = "自动追踪更新的任务",
+    ['SETTINGS_AUTO_TRACK_UPDATED_QUESTS_DESC'] = "如果一个任务被接受/更新，它将绕过过滤器并被自动追踪。\n\n|c00FF9696此功能用修改追踪来绕过过滤器。因此，禁用此选项也将清除您的设置。|r",
+
+    ['SETTINGS_SORTING_NAME'] = "排序",
+    ['SETTINGS_SORTING_DESC'] = "指示任务在追踪器中是如何排序的。",
+
+    ['SETTINGS_AUTO_HIDE_QUEST_HELPER_ICONS_NAME'] = "自动隐藏任务帮助图标",
+    ['SETTINGS_AUTO_HIDE_QUEST_HELPER_ICONS_DESC'] = "启用此功能将自动从支持的任务助手插件中隐藏图标。\n\n|c00FF9696支持的插件: %s",
+
+    ['SETTINGS_SORTING_DISABLED_OPTION'] = "不排序",
+    ['SETTINGS_SORTING_BY_LEVEL_OPTION'] = "级别",
+    ['SETTINGS_SORTING_BY_LEVEL_REVERSED_OPTION'] = "级别 (反向)",
+    ['SETTINGS_SORTING_BY_PERCENT_COMPLETED_OPTION'] = "完成百分比",
+    ['SETTINGS_SORTING_BY_RECENTLY_UPDATED_OPTION'] = "最近更新",
+    ['SETTINGS_SORTING_BY_QUEST_PROXIMITY_OPTION'] = "附近任务",
+
+    -- Settings 设置> Filters & Sorting 过滤和排序> Filters 过滤器
+
+    ['SETTINGS_DISABLE_FILTERS_NAME'] = "禁用过滤器 / 手动追踪任务",
+    ['SETTINGS_DISABLE_FILTERS_DESC'] = "禁用自动过滤功能,需要手动跟踪任务。\n\n|c00FF9696取消勾选将清除您的设置。|r",
+
+    ['SETTINGS_CURRENT_ZONE_ONLY_NAME'] = "仅当前区域",
+    ['SETTINGS_CURRENT_ZONE_ONLY_DESC'] = "显示与当前区域/子区域相关的任务。",
+
+    ['SETTINGS_QUEST_LIMIT_NAME'] = "任务限制",
+    ['SETTINGS_QUEST_LIMIT_DESC'] = "限制给定时间内屏幕上可见任务的数量。",
+
+    ['SETTINGS_HIDE_COMPLETED_QUESTS_NAME'] = "隐藏完成的任务",
+    ['SETTINGS_HIDE_COMPLETED_QUESTS_DESC'] = "显示已完成的任务。",
+
+    ['SETTINGS_RESET_TRACKING_OVERRIDES_NAME'] = "重置追踪设置",
+    ['SETTINGS_RESET_TRACKING_OVERRIDES_DESC'] = "重置的所有手动任务监视设置。",
+
+    -- Settings 设置> Visual Settings 视觉设置
+
+    ['SETTINGS_VISUAL_TAB'] = "视觉设置",
+
+    ['SETTINGS_FONT_SIZE_NAME'] = "字号",
+    ['SETTINGS_FONT_COLOR_NAME'] = "字体颜色",
+
+    ['SETTINGS_BACKGROUND_ALWAYS_VISIBLE_NAME'] = "背景始终可见",
+    ['SETTINGS_BACKGROUND_ALWAYS_VISIBLE_DESC'] = "追踪器的背景总是可见的。",
+
+    ['SETTINGS_BACKGROUND_COLOR_NAME'] = "背景色",
+    ['SETTINGS_BACKGROUND_COLOR_DESC'] = "追踪器的背景颜色。",
+
+    ['SETTINGS_QUEST_PADDING_NAME'] = "任务间距",
+
+    -- Settings 设置> Visual Settings 视觉设置> Tracker Header Settings 追踪器标题设置
+
+    ['SETTINGS_TRACKER_HEADER_ENABLED_DESC'] = "追踪器标题是否可见。",
+
+    ['SETTINGS_TRACKER_HEADER_FORMAT_DESC'] = "我们应该如何格式化追踪器标题?",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_OPTION'] = "任务",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_NUMBER_VISIBLE_OPTION'] = "任务 (10/17) -当前/自定追踪",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_NUMBER_VISIBLE_TOTAL_OPTION'] = "任务 (10/20) -当前/所有",
+
+    -- Settings 设置> Visual Settings 视觉设置> Zone Header Settings 区域标题设置
+
+    ['SETTINGS_ZONE_HEADER_ENABLED_DESC'] = "在任务上方添加可折叠区域标题。",
+
+    -- Settings 设置> Visual Settings 视觉设置> Quest Header Settings 任务标题设置
+
+    ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_NAME'] = "按难度级别着色",
+    ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_DESC'] = "根据难度来着色标题。",
+
+    ['SETTINGS_QUEST_HEADER_FORMAT_DESC'] = "对任务标题格式的精确控制。\n\n|c00FF9696{{title}}|r\n任务名称\n\n|c00FF9696{{level}}|r\n任务等级\n\n|c00FF9696{{zone}}|r\n任务所属区域",
+    ['SETTINGS_QUEST_HEADER_FORMAT_CLASSIC_OPTION'] = "经典",
+    ['SETTINGS_QUEST_HEADER_FORMAT_LEVELS_OPTION'] = "等级",
+
+    -- Settings 设置> Visual Settings 视觉设置> Objective Settings 目标设置
+
+    -- Settings 设置> Frame Settings 框体设置
+
+    ['SETTINGS_FRAME_TAB'] = "框体设置",
+
+    ['SETTINGS_LOCK_FRAME_NAME'] = "锁定框体",
+    ['SETTINGS_LOCK_FRAME_DESC'] = "防止框体被移动。",
+
+    ['SETTINGS_POSITIONX_NAME'] = "X轴位置(横向)",
+    ['SETTINGS_POSITIONY_NAME'] = "Y轴位置(纵向)",
+
+    ['SETTINGS_WIDTH_NAME'] = "宽度",
+    ['SETTINGS_MAX_HEIGHT_NAME'] = "最大高度",
+
+    ['SETTINGS_RESET_POSITION_NAME'] = "重置位置",
+
+    ['SETTINGS_RESET_SIZE_NAME'] = "重置大小",
+
+    -- Settings 设置> Advanced Settings 高级设置
+
+    ['SETTINGS_ADVANCED_TAB'] = "高级",
+
+    ['SETTINGS_DEVELOPER_HEADER'] = "开发者选项",
+    ['SETTINGS_DEVELOPER_MODE_NAME'] = "开发者模式",
+    ['SETTINGS_DEVELOPER_MODE_DESC'] = "启用日志记录和其他可视化帮助程序。",
+
+    ['SETTINGS_DEBUG_LEVEL_NAME'] = "调试级别",
+
+    ['SETTINGS_LOCALE_HEADER'] = "本地化设置",
+    ['SETTINGS_LOCALE_NAME'] = "选择UI(用户界面)缩放",
+
+    ['SETTINGS_RESET_HEADER'] = "重置Butter Quest Tracker",
+    ['SETTINGS_RESET_TEXT'] = "点击这个按钮将会重置所有的Butter Quest Tracker配置为默认值。",
+
+    ['SETTINGS_RESET_NAME'] = "重置设置",
+    ['SETTINGS_RESET_DESC'] = "将所有Butter Quest Tracker的设置重置为默认值。",
+    ['SETTINGS_ADVERT_TEXT'] = "|c00FF9696Butter Quest Tracker正在积极开发魔兽世界:经典怀旧服。请查看我们的GitHub的alpha版本或报告问题。\n\nhttps://github.com/butter-cookie-kitkat/ButterQuestTracker"
+};

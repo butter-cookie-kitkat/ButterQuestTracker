6457de1e1806bb0c38b14ef8aaecaa7b8625626d
Nick Woodward <nick@salte.io>
2019-12-16 17:42:16 -0600

    fix: added remaining russian translations courtesy of enllie

--- Locale/ruRU/config.lua
+++ Locale/ruRU/config.lua
@@ -2,7 +2,7 @@ ButterQuestTrackerLocale.locale['ruRU'] = {
     -- Quest Tracker
 
     ['QT_QUESTS'] = "Задания",
-    ['QT_READY_TO_TURN_IN'] = "Ready to turn in",
+    ['QT_READY_TO_TURN_IN'] = "Готов сдать",
     ['QT_FAILED'] = "Задание не выполнено.",
     ['QT_UNTRACK_QUEST'] = "Отменить отслеживание",
     ['QT_VIEW_QUEST'] = "Просмотреть",
@@ -18,44 +18,44 @@ ButterQuestTrackerLocale.locale['ruRU'] = {
     ['SETTINGS_FORMAT_NAME'] = "Формат",
 
     ['SETTINGS_DISPLAY_DUMMY_DATA_NAME'] = "Режим отладки",
-    ['SETTINGS_DISPLAY_DUMMY_DATA_DESC'] = "Displays dummy data in the tracker to allow you to tweak the visuals easier.\n\nThis also acts like your current zone is Thunder Bluff.",
+    ['SETTINGS_DISPLAY_DUMMY_DATA_DESC'] = "Отображает фиктивные данные в списке отслеживания, чтобы вы могли легче настроить визуальные эффекты.",
 
     -- Settings > Filters & Sorting
 
     ['SETTINGS_FILTERS_AND_SORTING_TAB'] = "Фильтры и сортировка",
 
     ['SETTINGS_AUTO_TRACK_UPDATED_QUESTS_NAME'] = "Автоматически отслеживать обновленные задания",
-    ['SETTINGS_AUTO_TRACK_UPDATED_QUESTS_DESC'] = "If a quest is accepted / updated it will bypass the filters and be automatically tracked.\n\n|c00FF9696This functionality utilizes the tracking overrides to bypass the filters. As such, disabling this option will also wipe out your overrides.|r",
+    ['SETTINGS_AUTO_TRACK_UPDATED_QUESTS_DESC'] = "Если задание принято / обновлено, оно обойдет стороной  фильтры и будет автоматически отслеживаться.\n\n|c00FF9696This functionality utilizes the tracking overrides to bypass the filters. As such, disabling this option will also wipe out your overrides.|r",
 
     ['SETTINGS_SORTING_NAME'] = "Сортировка",
-    ['SETTINGS_SORTING_DESC'] = "Dictate how quests are sorted in the tracker.",
+    ['SETTINGS_SORTING_DESC'] = "Как задания будут сортируются в списке отслеживания.",
 
     ['SETTINGS_AUTO_HIDE_QUEST_HELPER_ICONS_NAME'] = "Автоматически скрывать значки Quest Helper",
-    ['SETTINGS_AUTO_HIDE_QUEST_HELPER_ICONS_DESC'] = "Enabling this will automatically hide icons from supported Quest Helper Addons.\n\n|c00FF9696Supported Addons: %s",
+    ['SETTINGS_AUTO_HIDE_QUEST_HELPER_ICONS_DESC'] = "Включение этого пункта будет автоматически скрывать значки из поддерживаемых Quest Helper аддонов.\n\n|c00FF9696Supported Addons: %s",
 
     ['SETTINGS_SORTING_DISABLED_OPTION'] = "Не сортировать",
     ['SETTINGS_SORTING_BY_LEVEL_OPTION'] = "По уровню",
     ['SETTINGS_SORTING_BY_LEVEL_REVERSED_OPTION'] = "По уровню (Инверсия)",
     ['SETTINGS_SORTING_BY_PERCENT_COMPLETED_OPTION'] = "По %% Выполнения",
-    ['SETTINGS_SORTING_BY_RECENTLY_UPDATED_OPTION'] = "Недавно обновленные",
+    ['SETTINGS_SORTING_BY_RECENTLY_UPDATED_OPTION'] = "По Недавно обновленные",
     ['SETTINGS_SORTING_BY_QUEST_PROXIMITY_OPTION'] = "По близости",
 
     -- Settings > Filters & Sorting > Filters
 
     ['SETTINGS_DISABLE_FILTERS_NAME'] = "Отключить фильтры / Отслеживание заданий вручную",
-    ['SETTINGS_DISABLE_FILTERS_DESC'] = "Disables the automatic filtering functionality, requiring you to manually track quests.\n\n|c00FF9696Unchecking this will wipe out your overrides.|r",
+    ['SETTINGS_DISABLE_FILTERS_DESC'] = "Отключает функцию автоматической фильтрации, вам потребуется вручную отслеживать задания.\n\n|c00FF9696Снятие отметки с этого пункта сотрет добавленные вручную отслеживания.|r",
 
     ['SETTINGS_CURRENT_ZONE_ONLY_NAME'] = "Только текущая зона",
-    ['SETTINGS_CURRENT_ZONE_ONLY_DESC'] = "Displays quests relevant to the current zone / subzone.",
+    ['SETTINGS_CURRENT_ZONE_ONLY_DESC'] = "Отображает задания, относящиеся к текущей зоне / подзоне.",
 
     ['SETTINGS_QUEST_LIMIT_NAME'] = "Предел заданий",
-    ['SETTINGS_QUEST_LIMIT_DESC'] = "Limits the number of quests visible on the screen at a given time.",
+    ['SETTINGS_QUEST_LIMIT_DESC'] = "Ограничивает количество заданий, видимых на экране в данный момент.",
 
     ['SETTINGS_HIDE_COMPLETED_QUESTS_NAME'] = "Скрыть завершенные задания",
-    ['SETTINGS_HIDE_COMPLETED_QUESTS_DESC'] = "Displays quests that have been completed.",
+    ['SETTINGS_HIDE_COMPLETED_QUESTS_DESC'] = "Отображает задания, которые были выполнены.",
 
-    ['SETTINGS_RESET_TRACKING_OVERRIDES_NAME'] = "Сброс переопределений отслеживания",
-    ['SETTINGS_RESET_TRACKING_OVERRIDES_DESC'] = "Reset's all manual quest watch overrides.",
+    ['SETTINGS_RESET_TRACKING_OVERRIDES_NAME'] = "Сбросить отслеживаемые задания",
+    ['SETTINGS_RESET_TRACKING_OVERRIDES_DESC'] = "Сбросить все добавленные вручную отслеживания.",
 
     -- Settings > Visual Settings
 
@@ -65,39 +65,32 @@ ButterQuestTrackerLocale.locale['ruRU'] = {
     ['SETTINGS_FONT_COLOR_NAME'] = "Цвет шрифта",
 
     ['SETTINGS_BACKGROUND_ALWAYS_VISIBLE_NAME'] = "Фон всегда виден",
-    ['SETTINGS_BACKGROUND_ALWAYS_VISIBLE_DESC'] = "The background for the tracker will always be visible.",
+    ['SETTINGS_BACKGROUND_ALWAYS_VISIBLE_DESC'] = "Фон для списка отслеживания будет отображаться всегда.",
 
     ['SETTINGS_BACKGROUND_COLOR_NAME'] = "Цвет фона",
-    ['SETTINGS_BACKGROUND_COLOR_DESC'] = "The background color of the tracker.",
+    ['SETTINGS_BACKGROUND_COLOR_DESC'] = "Цвет фона списка отслеживания.",
 
     ['SETTINGS_QUEST_PADDING_NAME'] = "Отступ заданий",
 
     -- Settings > Visual Settings > Tracker Header Settings
 
-	['TRACKER_HEADER_SETTINGS'] = "Настройки заголовка отслеживания",
-	['SETTINGS_ZONE_HEADER'] = "Настройки заголовка зоны заданий",
-	['SETTINGS_QUEST_HEADER'] = "Настройки заголовка заданий",
-	['SETTINGS_OBJECTIV'] = "Настройки списка целей задания",
+    ['SETTINGS_TRACKER_HEADER_ENABLED_DESC'] = "Должен ли заголовок списка отслеживания быть видимым.",
 
-
-
-    ['SETTINGS_TRACKER_HEADER_ENABLED_DESC'] = "Whether the tracker header should be visible.",
-
-    ['SETTINGS_TRACKER_HEADER_FORMAT_DESC'] = "How should we format the tracker header?",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_DESC'] = "Оформление заголовока списка отслеживания?",
     ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_OPTION'] = "Задания",
     ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_NUMBER_VISIBLE_OPTION'] = "Задания (10/17)",
     ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_NUMBER_VISIBLE_TOTAL_OPTION'] = "Задания (10/20)",
 
     -- Settings > Visual Settings > Zone Header Settings
 
-    ['SETTINGS_ZONE_HEADER_ENABLED_DESC'] = "Adds collapsible zone headers above the quests.",
+    ['SETTINGS_ZONE_HEADER_ENABLED_DESC'] = "Добавляет сворачиваемые заголовки зоны над заданиями.",
 
     -- Settings > Visual Settings > Quest Header Settings
 
     ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_NAME'] = "Цвет по уровню сложности",
-    ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_DESC'] = "Colors the headers by their difficulty level.",
+    ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_DESC'] = "Цвет заголовков по уровню сложности.",
 
-    ['SETTINGS_QUEST_HEADER_FORMAT_DESC'] = "Fine-grained control over the quest header formatting.\n\n|c00FF9696{{title}}|r\nName of the quest.\n\n|c00FF9696{{level}}|r\nThe level of the quest\n\n|c00FF9696{{zone}}|r\nThe zone the quest belongs to",
+    ['SETTINGS_QUEST_HEADER_FORMAT_DESC'] = "Тонкий контроль над оформлением заголовка задания.\n\n|c00FF9696{{title}}|r\nНазвание задания.\n\n|c00FF9696{{level}}|r\nУровень задания\n\n|c00FF9696{{zone}}|r\nЗона, к которой относится задание",
     ['SETTINGS_QUEST_HEADER_FORMAT_CLASSIC_OPTION'] = "Classic",
     ['SETTINGS_QUEST_HEADER_FORMAT_LEVELS_OPTION'] = "Levels",
 
@@ -108,7 +101,7 @@ ButterQuestTrackerLocale.locale['ruRU'] = {
     ['SETTINGS_FRAME_TAB'] = "Настройки окна",
 
     ['SETTINGS_LOCK_FRAME_NAME'] = "Заблокировать окно",
-    ['SETTINGS_LOCK_FRAME_DESC'] = "Prevents the frame from being moved.",
+    ['SETTINGS_LOCK_FRAME_DESC'] = "Предотвращает перемещение окна.",
 
     ['SETTINGS_POSITIONX_NAME'] = "Позиция X",
     ['SETTINGS_POSITIONY_NAME'] = "Позиция Y",
@@ -126,7 +119,7 @@ ButterQuestTrackerLocale.locale['ruRU'] = {
 
     ['SETTINGS_DEVELOPER_HEADER'] = "Опции разработчика",
     ['SETTINGS_DEVELOPER_MODE_NAME'] = "Режим разработчика",
-    ['SETTINGS_DEVELOPER_MODE_DESC'] = "Enables logging and other visual helpers.",
+    ['SETTINGS_DEVELOPER_MODE_DESC'] = "Включает ведение журнала и другие визуальные помощники.",
 
     ['SETTINGS_DEBUG_LEVEL_NAME'] = "Уровень отладки",
 
@@ -134,9 +127,9 @@ ButterQuestTrackerLocale.locale['ruRU'] = {
     ['SETTINGS_LOCALE_NAME'] = "Выберете язык UI",
 
     ['SETTINGS_RESET_HEADER'] = "Сбросить Butter Quest Tracker",
-    ['SETTINGS_RESET_TEXT'] = "Hitting this button will reset all Butter Quest Tracker configuration settings back to their default values.",
+    ['SETTINGS_RESET_TEXT'] = "При нажатии на эту кнопку все настройки конфигурации Butter Quest Tracker вернутся к значениям по умолчанию.",
 
     ['SETTINGS_RESET_NAME'] = "Сбросить настройки",
-    ['SETTINGS_RESET_DESC'] = "Reset all of Butter Quest Tracker's settings to their default values.",
+    ['SETTINGS_RESET_DESC'] = "Сброс всех настроек Butter Quest Tracker до значений по умолчанию.",
     ['SETTINGS_ADVERT_TEXT'] = "|c00FF9696Butter Quest Tracker is under active development for World of Warcraft: Classic. Please check out our GitHub for the alpha builds or to report issues. \n\nhttps://github.com/butter-cookie-kitkat/ButterQuestTracker"
 };
